b1ece490e6a97f55bb3ac8e87ceda5a9a98b1247
mpq197 <mpq197@gmail.com>
2026-03-02 13:53:46 +0800

    Create fixing.js

---
+++ tools/fixing.js
@@ -0,0 +1,16 @@
+// tools/fixing.js
+// updated: 2026-03-02
+
+export function render(){
+  return `
+    <div class="container mt-2">
+      <div class="card">
+        <div class="card-header text-center">fixing</div>
+        <div class="card-body">
+          <div class="text-muted">修改中，請先使用右下方舊版按鈕</div>
+        </div>
+      </div>
+    </div>
+  `;
+}
+export function init(){ }
